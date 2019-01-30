ab5257d08a391a7275cc053f3ff11ef9751aa1d8
Sun Lee <hi@sunlee.io>
2019-01-30 00:49:16 -0500

    Initial commit

---
+++ BinaryTreeBFS.js
@@ -0,0 +1,81 @@
+class BinaryTreeBFS {
+	run(tree, value) {
+		if (!tree) return new Error("Tree is a required field")
+
+		let found
+		let queue = [tree]
+		let permQueue = []
+
+		while (queue.length) {
+			const node = queue[0]
+
+			if (node.value === value) {
+				found = node.name
+				queue = []
+			} else {
+				permQueue.push(node.name)
+				queue.shift()
+				!!node.left && queue.push(node.left)
+				!!node.right && queue.push(node.right)
+			}
+		}
+
+		console.log(permQueue)
+
+		return found || "Does not exist"
+	}
+}
+
+/*****************/
+/* SAMPLE RUNNER */
+/*****************/
+
+let tree = {
+	name: 'A',
+	value: 4,
+	left: {
+		name: 'B', 
+		value: 7,
+		left: {
+			name: 'D', 
+			value: 9,
+			left: null,
+			right: null,
+		},
+		right: {
+			name: 'E', 
+			value: 65,
+			left: {
+				name: 'H', 
+				value: 21, 
+				left: null,
+				right: null,
+			},
+			right: {
+				name: 'I', 
+				value: 33, 
+				left: null,
+				right: null,
+			},
+		},
+	},
+	right: {
+		name: 'C', 
+		value: 11,
+		left: {
+			name: 'F', 
+			value: 9, 
+			left: null,
+			right: null,
+		},
+		right: {
+			name: 'G', 
+			value: 55, 
+			left: null,
+			right: null,
+		},
+	},
+};
+
+const btbfs = new BinaryTreeBFS();
+btbfs.run(tree, 33);
---
+++ TreeBFS.js
@@ -0,0 +1,61 @@
+class TreeBFS {
+    run(tree, value) {
+        if (!tree) return new Error("Tree is a required field")
+
+        let found
+        let queue = [tree]
+        let permQueue = [tree.name]
+
+        while (queue.length) {
+            for (let i = 0; i < queue.length; i++) {
+                let node = queue[i]
+
+                if (node.value === value)  {
+                    found = node.name
+                    queue = []
+                } else {
+                    queue.shift()
+
+                    for (let j = 0; j < node.children.length; j++) {
+                        permQueue.push(node.children[j].name)
+                        queue.push(node.children[j])
+                    }
+                }
+            }
+        }
+
+        console.log("permQueue: ", permQueue);
+
+        return found || "Does not exist"
+    }
+}
+
+/*****************/
+/* SAMPLE RUNNER */
+/*****************/
+
+let tree = {
+    name: 'A',
+    value: 4,
+    children: [
+        {
+            name: 'B', value: 7,
+            children: [{name: 'F', value: 9, children: []}]
+        },
+        {
+            name: 'C', value: 11,
+            children: [{name: 'G', value: 9, children: []}]
+        },
+        {name: 'D', value: 55, children: []},
+        {
+            name: 'E', value: 65,
+            children: [
+                {name: 'H', value: 21, children: []},
+                {name: 'I', value: 33, children: []}
+            ]
+        }
+    ]
+};
+
+const tbfs = new TreeBFS();
+tbfs.run(tree, 33);
